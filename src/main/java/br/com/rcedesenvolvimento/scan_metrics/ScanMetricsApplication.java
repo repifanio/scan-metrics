19798fac1952e266793d0e3f04b26c904f5108fe
Rafa Epifanio <rafael.cepifanio@gmail.com>
2024-08-19 16:56:31 -0300

    init project

---
+++ src/main/java/br/com/rcedesenvolvimento/scan_metrics/ScanMetricsApplication.java
@@ -0,0 +1,13 @@
+package br.com.rcedesenvolvimento.scan_metrics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScanMetricsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScanMetricsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/rcedesenvolvimento/scan_metrics/ScanMetricsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rcedesenvolvimento.scan_metrics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScanMetricsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
